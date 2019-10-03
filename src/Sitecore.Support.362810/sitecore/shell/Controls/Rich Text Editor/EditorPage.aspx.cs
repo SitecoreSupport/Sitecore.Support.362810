17df45c6274a7bf3b331ab872889df8ec19f443a
sc-ArtemKosohov <ark@sitecore.net>
2019-10-03 14:18:22 +0300

    fix

---
+++ src/Sitecore.Support.362810/sitecore/shell/Controls/Rich Text Editor/EditorPage.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sitecore.Support.sitecore.shell.Controls.Rich_Text_Editor
+{
+    public partial class EditorPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
